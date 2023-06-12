e1d2e8cdd26e5a84b47a2e122c79d6069c0ab91d
Daniel Obregon <danobrey@gmail.com>
2023-06-12 09:38:21 +0200

    Switch 'Product' endpoint to ReadOnlyModelViewSet

--- project/urls.py
+++ project/urls.py
@@ -2,15 +2,14 @@ from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
 
-from shop.views import CategoryViewset, ProductView
-
+from shop.views import CategoryViewset, ProductViewSet
 
 router = routers.SimpleRouter()
 router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/product/', ProductView.as_view()),
     path('api/', include(router.urls))
 ]
