a9401e2dc1763f8b0bb957c23b2250c85e3b76e1
Daniel Obregon <danobrey@gmail.com>
2023-06-12 09:33:23 +0200

    add 'product-list' endpoint

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from shop.views import CategoryView
-
+from shop.views import CategoryView, ProductListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryView.as_view())
+    path('api/category/', CategoryView.as_view()),
+    path('api/product/', ProductListView.as_view(), "product-list"),
 ]
